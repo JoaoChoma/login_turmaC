b8d13c9eeb2a2d95d05b5f82d61451185f1500d8
JoaoChoma <jchomax@gmail.com>
2025-05-26 22:26:35 -0300

    listar()

--- js/cadastro.js
+++ js/cadastro.js
@@ -11,6 +11,28 @@ botao.addEventListener('click', function (){
     listaUsuariosCadastrados.push(objUsuario);
     const listaJson = JSON.stringify(listaUsuariosCadastrados);
     localStorage.setItem('usuarios', listaJson);
+    listar();
 }
 
 );
+
+//listar
+function listar(){
+    const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    let tabela = document.getElementById('listaUsuariosCadastrados');
+    tabela.innerHTML = "";
+    listaUsuarios.forEach((objeto, index) => {
+        let linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${objeto.usuario}</td>
+            <td>${objeto.senha}</td>
+            <td>
+                <button>Editar</button>
+                <button>Remover</button>
+            </td>
+        `;
+        tabela.appendChild(linha);
+    });
+}
+
+listar();
