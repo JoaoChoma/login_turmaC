562a60b5cedabf27ec0800e38d1e984c9f637015
JoaoChoma <jchomax@gmail.com>
2025-06-02 22:23:25 -0300

    editar finalizado - login ok

--- js/login.js
+++ js/login.js
@@ -1,1 +1,26 @@
-// para fazer login
+// login com validacao
+const entrar = document.getElementById('entrar');
+
+entrar.addEventListener('click',
+    function (){
+        let login = document.getElementById('login').value;
+        let senha = document.getElementById('senha').value;
+
+        // memoria local
+        const usuarioCadastrados = JSON.parse(
+            localStorage.getItem("usuarios")
+        ) || [];
+
+        let usuarioEncotrado = usuarioCadastrados.find(
+            u => u.usuario === login && u.senha === senha
+            // function (){}
+            // () => {}    
+        );
+        if(usuarioEncotrado){
+            localStorage.setItem("userLogado", login);
+            window.location.href = "dashboard.html";
+        }else{
+            document.getElementById('mensagem').textContent = "Login ou senha incorretos";
+        }
+    }
+);
